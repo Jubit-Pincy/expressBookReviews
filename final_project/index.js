64cf786a3870d00c39a463f1c2457d33ccf85e96
Jubit-pincy <jubitpincy@gmail.com>
2024-09-29 10:56:08 -0400

    Task 7 done

--- final_project/index.js
+++ final_project/index.js
@@ -11,12 +11,18 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
+    const username = req.body.username;
+    const password = req.body.password;
+    // Check if username or password is missing
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+    // Authenticate user
     if (authenticatedUser(username, password)) {
         // Generate JWT access token
         let accessToken = jwt.sign({
             data: password
-        }, 'access', { expiresIn: 60 });
-
+        }, 'access', { expiresIn: 60 * 60 });
         // Store access token and username in session
         req.session.authorization = {
             accessToken, username
